b87477b932161195d4f8d1ad2f0d30d690cc5bef
Bluey Code <blueycode@gmail.com>
2023-01-23 21:07:57 -0300

    feat: develop interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Logo from "./assets/logo.png";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div id="logo">
+        <img src={Logo} />
+        <h2>MP3 DOWNLOADER</h2>
+      </div>
+
+      <div id="body">
+        <input
+          type="text"
+          placeholder="YouTube link here"
+        />
+        <span>It might take a moment to convert your video</span>
+      </div>
+
+      <button>Download</button>
+    </div>
   )
 }
 
